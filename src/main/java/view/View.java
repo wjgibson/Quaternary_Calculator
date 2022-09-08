9bece6b0bdc45e01a770dece40767fdc4ab6dd95
melody <melodylynnnh@gmail.com>
2022-09-08 12:37:06 -0400

    update start method to work

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -6,11 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 
 public class View extends Application{
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Calc.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("Calc.fxml")));
         
         Scene scene = new Scene(root, 400, 800);
     
