5750d061b259f91055f4d1725b091fa99540c677
Jacob Dye <jacobadye1@gmail.com>
2022-09-10 18:36:59 -0400

    Add wip handleTwoButton

--- src/main/java/view/Controller.java
+++ src/main/java/view/Controller.java
@@ -4,47 +4,69 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import model.Calculator;
+
 
 import java.util.Objects;
 
 public class Controller {
+    Calculator calculator = new Calculator();
 
-    @FXML private Text display; 
+    @FXML private Text display;
 
     @FXML
-    public void handleButton(MouseEvent mouseEvent) {
-        Button target = ((Button)mouseEvent.getSource());
-        String input1 = "";
-        String input2 = "";
-        char operand;
-        /*
-        if (target.getText().equals("0") || target.getText().equals("1")
-                || target.getText().equals("2") || target.getText().equals("3")) {
-            input1 = target.getText();
-            if (input1.!equals("")) {
-                input2 = target.getText();
-            }
-
-            }
-        else if (target.getText().equals("=")){
-            display.setText(display.getText() + target.getText());
+    public void handleTwoButton(MouseEvent mouseEvent) {
+        Button target = ((Button) mouseEvent.getSource());
+        if (calculator.getOperands() == null){
+            calculator.setOperands(target.getText(), null);
         }
+        else {
+            calculator.setOperands("2", "2");
+        }
+        display.setText(display + target.getText());
+    }
+    public void handleThreeButton(MouseEvent mouseEvent) {
 
-         */
     }
 
-    public String[] handleNumberButton(MouseEvent mouseEvent, String input1, String input2) {
-        Button target = ((Button)mouseEvent.getSource());
-        if (input1.equals("")) {
-            input1 = target.getText();
-        } else {
-            input2 = target.getText();
-        }
-        String[] s = new String[2];
-        s[0] = input1;
-        s[1] = input2;
-        return s;
+    public void handleOneButton(MouseEvent mouseEvent) {
+    }
 
+    public void handlePlusButton(MouseEvent mouseEvent) {
+        calculator.setOperator('+');
+    }
+
+    public void handleMinusButton(MouseEvent mouseEvent) {
+        calculator.setOperator('-');
+    }
+
+    public void handleTimesButton(MouseEvent mouseEvent) {
+        calculator.setOperator('*');
+    }
+
+    public void handleRootButton(MouseEvent mouseEvent) {
+        calculator.setOperator('r');
+    }
+
+    public void handleSquareButton(MouseEvent mouseEvent) {
+        calculator.setOperator('s');
+    }
+
+    public void handleDivideButton(MouseEvent mouseEvent) {
+        calculator.setOperator('/');
+    }
+
+    public void handleConvertButton(MouseEvent mouseEvent) {
+    }
+
+    public void handleClearButton(MouseEvent mouseEvent) {
+    }
+
+    public void handleZeroButton(MouseEvent mouseEvent) {
+    }
+
+    public void handleEqualsButton(MouseEvent mouseEvent) {
+        calculator.calculate();
     }
 }
 
